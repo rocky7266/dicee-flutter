30c6e8c05d89e84dcf3e41280d9303310f3a3ba6
Labnan <labnan420@gmail.com>
2020-10-27 21:42:36 +0600

    Added text widget
    Text widget added inside container.

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text('asdasd'),
+    );
   }
 }
