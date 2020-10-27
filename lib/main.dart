4bd7eb3430c9dad5df0b003814b315d58df145dc
Labnan <57247333+rocky7266@users.noreply.github.com>
2020-10-27 21:44:06 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('asdasd'),
+      child: Text('hello'),
     );
   }
 }
